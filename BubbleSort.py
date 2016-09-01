6d03b54f32b8c322daa254aaf04f6a5ef548fd7b
argowtham <gowtayna@indiana.edu>
2016-09-01 14:20:01 -0400

    Created a function to swap the two elements

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,11 +1,14 @@
+def swap(a, b):
+    return b, a
+
+
 def bubble_sort(arr_in):
     for i in range(0, len(arr_in)):
         for j in range(0, len(arr_in)-1-i):
             if arr_in[j] > arr_in[j+1]:
-                tmp = arr_in[j]
-                arr_in[j] = arr_in[j+1]
-                arr_in[j+1] = tmp
+                arr_in[j], arr_in[j+1] = swap(arr_in[j], arr_in[j+1])
     print("Array Sorted by Bubble sort: ", arr_in)
+
 
 if __name__ == '__main__':
     print("Program to demonstrate Bubble sort")
