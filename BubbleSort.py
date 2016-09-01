29fba8cd02609a27b39e0c68164840ffeb3e743d
argowtham <gowtayna@indiana.edu>
2016-09-01 14:16:17 -0400

    Bubble sort algorithm

---
+++ BubbleSort.py
@@ -0,0 +1,19 @@
+def bubble_sort(arr_in):
+    for i in range(0, len(arr_in)):
+        for j in range(0, len(arr_in)-1-i):
+            if arr_in[j] > arr_in[j+1]:
+                tmp = arr_in[j]
+                arr_in[j] = arr_in[j+1]
+                arr_in[j+1] = tmp
+    print("Array Sorted by Bubble sort: ", arr_in)
+
+if __name__ == '__main__':
+    print("Program to demonstrate Bubble sort")
+    n = int(input("Enter the number of elements"))
+    print("Enter the elements one by one")
+    arr = []
+    for i in range(0, n):
+        arr.append(int(input()))
+    print("Input array: ", arr)
+    # Calling insertion sort algorithm
+    bubble_sort(arr)
